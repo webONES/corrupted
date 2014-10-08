f472c653ad94ed104687c86259b34dade7625f56
joseramonc <joserracamacho@gmail.com>
2014-10-08 14:30:33 -0500

    generate file with random bytes inside

--- lib/corrupted.rb
+++ lib/corrupted.rb
@@ -1,12 +1,17 @@
 require "corrupted/version"
+require "base64"
 
 module Corrupted
-  def self.create name, extension
+  ### name: The name of the file to create
+  ### extension: The extension of the file to create
+  ### size [optional]: The size (in kilobytes) of the file to create
+
+  def self.create name, extension, size=64
     if File.exist?("#{name}.#{extension}")
       puts "File alredy exists :("
     else
       File.open "#{name}.#{extension}", "w" do |f|
-        f.puts "hola"
+        f.puts Random.new.bytes(size*1000) #convert kilobytes to bytes
       end
     end
   end
